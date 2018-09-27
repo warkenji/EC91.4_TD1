927debfea912e1fbf44746f36e4c7c2b1ca78663
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-27 02:13:49 +0200

    création d'un model basé sur la classe, l'ordre, la famille et les noms commun et scientifique des animaux

--- project/ifap/models.py
+++ project/ifap/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Classe(models.Model):
+    nom = models.CharField(max_length=255)
+
+
+class Ordre(models.Model):
+    nom = models.CharField(max_length=255)
+    classe = models.ForeignKey(Classe, models.CASCADE)
+
+
+class Famille(models.Model):
+    nom = models.CharField(max_length=255)
+    ordre = models.ForeignKey(Ordre, models.CASCADE)
+
+
+class Animal(models.Model):
+    nom_commun = models.CharField(max_length=255)
+    nom_scientifique = models.CharField(max_length=255)
+    famille = models.ForeignKey(Famille, models.CASCADE)
