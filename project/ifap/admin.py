428123f2925ff63373a0f5a3e34fdbf51f8623a5
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-27 22:25:16 +0200

    Ajout de modèles d'affichage personnalisé dans 'admin'

--- project/ifap/admin.py
+++ project/ifap/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import Classe, Ordre, Famille, Animal
 
-admin.site.register(Classe)
-admin.site.register(Ordre)
-admin.site.register(Famille)
-admin.site.register(Animal)
+
+@admin.register(Classe)
+class ClasseAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+    ordering = ['nom']
+
+
+@admin.register(Ordre)
+class OrdreAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'classe']
+    list_filter = ['classe']
+    ordering = ['nom']
+
+
+@admin.register(Famille)
+class FamilleAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'ordre', 'classe']
+    list_filter = ['ordre__classe', 'ordre']
+    ordering = ['nom']
+
+    def classe(self, obj):
+        return obj.ordre.classe
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ['nom_scientifique', 'nom_commun', 'famille', 'ordre', 'classe']
+    list_filter = ['famille__ordre__classe', 'famille__ordre', 'famille']
+    ordering = ['nom_scientifique', 'nom_commun']
+
+    def ordre(self, obj):
+        return obj.famille.ordre
+
+    def classe(self, obj):
+        return obj.famille.ordre.classe
+
