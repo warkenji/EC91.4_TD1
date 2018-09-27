89ba30d6f80b74287c09b1db0387875ffeaab18c
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-27 02:25:54 +0200

    Ajout des éléments de la base de donnée à la partie 'admin'

--- project/ifap/admin.py
+++ project/ifap/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.register(Classe)
+admin.register(Ordre)
+admin.register(Famille)
+admin.register(Animal)
