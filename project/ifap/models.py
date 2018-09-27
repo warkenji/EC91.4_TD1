d21e0470dc32a9e3bce78fb7560f4ad3a01a6255
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-27 05:35:54 +0200

    changement de la chaine de caractère affiché par chaque modele

--- project/ifap/models.py
+++ project/ifap/models.py
@@ -6,18 +6,30 @@ from django.db import models
 class Classe(models.Model):
     nom = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nom
+
 
 class Ordre(models.Model):
     nom = models.CharField(max_length=255)
     classe = models.ForeignKey(Classe, models.CASCADE)
+
+    def __str__(self):
+        return self.nom
 
 
 class Famille(models.Model):
     nom = models.CharField(max_length=255)
     ordre = models.ForeignKey(Ordre, models.CASCADE)
 
+    def __str__(self):
+        return self.nom
+
 
 class Animal(models.Model):
     nom_commun = models.CharField(max_length=255)
     nom_scientifique = models.CharField(max_length=255)
     famille = models.ForeignKey(Famille, models.CASCADE)
+
+    def __str__(self):
+        return '%s (%s)' % (self.nom_commun, self.nom_scientifique)
