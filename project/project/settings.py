54335c94ab1d71a40df802f42d889732519fd4ae
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-29 21:20:26 +0200

    Import de Jinja2

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,6 +54,14 @@ ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'project.jinja2.environment'
+        },
+    },
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
