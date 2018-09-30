1170c1548ebc21fea1459dd0b13e51325e0733f9
Warren Gaspard <warren.gsprd@gmail.com>
2018-09-30 14:43:47 +0200

    Ajout de l'affichage des classes

--- project/ifap/views.py
+++ project/ifap/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Classe, Ordre, Famille, Animal
 
 
 def index(request):
-    context = {}
+    context = {
+        'classes': Classe.objects.all()
+    }
+
     return render(request, 'ifap/index.html', context)
