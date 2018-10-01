1d0dc4e04dc273440ede773aef89c93123bb1d29
Warren Gaspard <warren.gsprd@gmail.com>
2018-10-01 09:55:58 +0200

    changement des noms lié à chaque model en clé étrangère

--- project/ifap/models.py
+++ project/ifap/models.py
@@ -12,7 +12,7 @@ class Classe(models.Model):
 
 class Ordre(models.Model):
     nom = models.CharField(max_length=255)
-    classe = models.ForeignKey(Classe, models.CASCADE)
+    classe = models.ForeignKey(Classe, models.CASCADE, 'ordres')
 
     def __str__(self):
         return self.nom
@@ -20,7 +20,7 @@ class Ordre(models.Model):
 
 class Famille(models.Model):
     nom = models.CharField(max_length=255)
-    ordre = models.ForeignKey(Ordre, models.CASCADE)
+    ordre = models.ForeignKey(Ordre, models.CASCADE, 'familles')
 
     def __str__(self):
         return self.nom
@@ -29,7 +29,7 @@ class Famille(models.Model):
 class Animal(models.Model):
     nom_commun = models.CharField(max_length=255)
     nom_scientifique = models.CharField(max_length=255)
-    famille = models.ForeignKey(Famille, models.CASCADE)
+    famille = models.ForeignKey(Famille, models.CASCADE, 'animaux')
 
     def __str__(self):
         return '%s (%s)' % (self.nom_commun, self.nom_scientifique)
